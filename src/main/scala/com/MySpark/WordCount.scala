7d71ec5735f7cceb214081e6b23fdecd2acc82a6
shiyun.chen <shiyun.chen@ucloud.cn>
2018-10-26 15:26:07 +0800

    add init file

---
+++ src/main/scala/com/MySpark/WordCount.scala
@@ -0,0 +1,28 @@
+package com.MySpark
+
+import org.apache.spark.mllib.linalg.{Matrices, Matrix}
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by Administrator on 2018/10/26.
+  * Joseph.Chen
+  */
+
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf=new SparkConf()
+      .setMaster("local")//启动本地化计算
+      .setAppName("testRdd")//设置本程序名称
+
+    //Spark程序的编写都是从SparkContext开始的
+    val sc=new SparkContext(conf)
+    //以上的语句等价与val sc=new SparkContext("local","testRdd")
+    val data=sc.textFile("/Users/Chensy/test")//读取本地文件
+    data.flatMap(_.split(" "))//下划线是占位符，flatMap是对行操作的方法，对读入的数据进行分割
+      .map((_,1))//将每一项转换为key-value，数据是key，value是1
+      .reduceByKey(_+_)//将具有相同key的项相加合并成一个
+      .collect()//将分布式的RDD返回一个单机的scala array，在这个数组上运用scala的函数操作，并返回结果到驱动程序
+      .foreach(println)//循环打印
+  }
+}
